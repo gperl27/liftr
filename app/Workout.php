5b8d8a323695c701431a5f59e6ab313f9f299333
Greg P <gperlman27@gmail.com>
2016-11-13 16:35:21 -0500

    setup schema and model relationships

---
+++ app/Workout.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\User;
+use App\Exercise;
+
+
+class Workout extends Model
+{
+    protected $fillable = [
+        'day', 'week'
+    ];
+
+    public function user(){
+    	return $this->belongsTo(User::class);
+    }
+
+    public function exercises(){
+    	return $this->hasMany(Exercise::class);
+    }
+}
