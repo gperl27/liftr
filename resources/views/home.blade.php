b0a5f91d8015b733135dde0c3ac9590915e3d84b
Greg P <gperlman27@gmail.com>
2016-11-16 21:12:26 -0500

    begin styling home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,96 +11,162 @@
         <a class='btn btn-info' href="/workouts/new">Add Workout +</a>
     </div>
 </div>
-<div class='row'>	
-    <div class='col-md-2 col-md-offset-1'>
-    	<h2>Monday</h2>
-        @if($mondayWorkout)
-        	<a href="/workouts/{{ $mondayWorkout->id }}">Edit</a>
-        	<ul>
-        	@foreach($mondayExercises as $exercise)
-        		<li>
-    	    		<h4>{{ $exercise->name }}</h4>
-    	    		<span>Sets: {{ $exercise->sets }}</span>
-    	    		<span>Reps: {{ $exercise->reps }}</span>
-    	    		<span>Weight: {{ $exercise->weight }}</span>
-        		</li>
-        	@endforeach
-        	</ul>
-        @else
-            <h4>No Monday workout found for this week.</h4>
-        @endif
-    </div>
-    <div class='col-md-2'>
-    	<h2>Tuesday</h2>
-        @if($tuesdayWorkout)
-            <a href="/workouts/{{ $tuesdayWorkout->id }}">Edit</a>
-            <ul>
-            @foreach($tuesdayExercises as $exercise)
-                <li>
-                    <h4>{{ $exercise->name }}</h4>
-                    <span>Sets: {{ $exercise->sets }}</span>
-                    <span>Reps: {{ $exercise->reps }}</span>
-                    <span>Weight: {{ $exercise->weight }}</span>
-                </li>
-            @endforeach
-            </ul>
-        @else
-            <h4>No Tuesday workout found for this week.</h4>
-        @endif
-    </div>
-    <div class='col-md-2'>
-    	<h2>Wednesday</h2>
-        @if($wednesdayWorkout)
-            <a href="/workouts/{{ $wednesdayWorkout->id }}">Edit</a>
-            <ul>
-            @foreach($wednesdayExercises as $exercise)
-                <li>
-                    <h4>{{ $exercise->name }}</h4>
-                    <span>Sets: {{ $exercise->sets }}</span>
-                    <span>Reps: {{ $exercise->reps }}</span>
-                    <span>Weight: {{ $exercise->weight }}</span>
-                </li>
-            @endforeach
-            </ul>
-        @else
-            <h4>No Wednesday workout found for this week.</h4>
-        @endif
-    </div>
-    <div class='col-md-2'>
-    	<h2>Thursday</h2>
-        @if($thursdayWorkout)
-            <a href="/workouts/{{ $thursdayWorkout->id }}">Edit</a>
-            <ul>
-            @foreach($thursdayExercises as $exercise)
-                <li>
-                    <h4>{{ $exercise->name }}</h4>
-                    <span>Sets: {{ $exercise->sets }}</span>
-                    <span>Reps: {{ $exercise->reps }}</span>
-                    <span>Weight: {{ $exercise->weight }}</span>
-                </li>
-            @endforeach
-            </ul>
-        @else
-            <h4>No Thursday workout found for this week.</h4>
-        @endif
-    </div>
-    <div class='col-md-2'>
-    	<h2>Friday</h2>
-        @if($fridayWorkout)
-            <a href="/workouts/{{ $fridayWorkout->id }}">Edit</a>
-            <ul>
-            @foreach($fridayExercises as $exercise)
-                <li>
-                    <h4>{{ $exercise->name }}</h4>
-                    <span>Sets: {{ $exercise->sets }}</span>
-                    <span>Reps: {{ $exercise->reps }}</span>
-                    <span>Weight: {{ $exercise->weight }}</span>
-                </li>
-            @endforeach
-            </ul>
-        @else
-            <h4>No Friday workout found for this week.</h4>
-        @endif
+<div class='container-fluid'>
+    <div class='row'>   
+        <div class='col-md-2 col-md-offset-1'>
+            <h2 class='home-header'>Monday</h2>
+            @if($mondayWorkout)
+                <a class='btn btn-warning home-edit-btn' href="/workouts/{{ $mondayWorkout->id }}"><span class='glyphicon glyphicon-pencil' aria-hidden="true"></span></a>
+                <ul class='list-group'>
+                @foreach($mondayExercises as $exercise)
+                    <li class='list-group-item'>
+                        <h4 class='h4-home'>{{ $exercise->name }}</h4>
+                        <table class='table table-condensed'>
+                            <thead>
+                                <tr>
+                                    <th>Sets</th>
+                                    <th>Reps</th>
+                                    <th>Weight (lbs)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $exercise->sets }}</td>
+                                    <td>{{ $exercise->reps }}</td>
+                                    <td>{{ $exercise->weight }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                @endforeach
+                </ul>
+            @else
+                <h4 class='h4-home alert alert-info'>No Monday workout found for this week.</h4>
+            @endif
+        </div>
+        <div class='col-md-2'>
+            <h2 class='home-header'>Tuesday</h2>
+            @if($tuesdayWorkout)
+                <a class='btn btn-warning home-edit-btn' href="/workouts/{{ $tuesdayWorkout->id }}"><span class='glyphicon glyphicon-pencil' aria-hidden="true"></span></a>
+                <ul class='list-group'>
+                @foreach($tuesdayExercises as $exercise)
+                    <li class='list-group-item'>
+                        <h4 class='h4-home'>{{ $exercise->name }}</h4>
+                        <table class='table table-condensed'>
+                            <thead>
+                                <tr>
+                                    <th>Sets</th>
+                                    <th>Reps</th>
+                                    <th>Weight (lbs)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $exercise->sets }}</td>
+                                    <td>{{ $exercise->reps }}</td>
+                                    <td>{{ $exercise->weight }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                @endforeach
+                </ul>
+            @else
+                <h4 class='h4-home alert alert-info'>No Tuesday workout found for this week.</h4>
+            @endif
+        </div>
+        <div class='col-md-2'>
+            <h2 class='home-header'>Wednesday</h2>
+            @if($wednesdayWorkout)
+                <a class='btn btn-warning home-edit-btn' href="/workouts/{{ $wednesdayWorkout->id }}"><span class='glyphicon glyphicon-pencil' aria-hidden="true"></span></a>
+                <ul class='list-group'>
+                @foreach($wednesdayExercises as $exercise)
+                    <li class='list-group-item'>
+                        <h4 class='h4-home'>{{ $exercise->name }}</h4>
+                        <table class='table table-condensed'>
+                            <thead>
+                                <tr>
+                                    <th>Sets</th>
+                                    <th>Reps</th>
+                                    <th>Weight (lbs)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $exercise->sets }}</td>
+                                    <td>{{ $exercise->reps }}</td>
+                                    <td>{{ $exercise->weight }}</td>
+                                </tr>
+                            </tbody>
+                        </table>                    
+                    </li>
+                @endforeach
+                </ul>
+            @else
+                <h4 class='h4-home alert alert-info'>No Wednesday workout found for this week.</h4>
+            @endif
+        </div>
+        <div class='col-md-2'>
+        	<h2 class='home-header'>Thursday</h2>
+            @if($thursdayWorkout)
+                <a class='btn btn-warning home-edit-btn' href="/workouts/{{ $thursdayWorkout->id }}"><span class='glyphicon glyphicon-pencil' aria-hidden="true"></span></a>
+                <ul class='list-group'>
+                @foreach($thursdayExercises as $exercise)
+                    <li class='list-group-item'>
+                        <h4 class='h4-home'>{{ $exercise->name }}</h4>
+                        <table class='table table-condensed'>
+                            <thead>
+                                <tr>
+                                    <th>Sets</th>
+                                    <th>Reps</th>
+                                    <th>Weight (lbs)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $exercise->sets }}</td>
+                                    <td>{{ $exercise->reps }}</td>
+                                    <td>{{ $exercise->weight }}</td>
+                                </tr>
+                            </tbody>
+                        </table>                    
+                    </li>
+                @endforeach
+                </ul>
+            @else
+                <h4 class='h4-home alert alert-info'>No Thursday workout found for this week.</h4>
+            @endif
+        </div>
+        <div class='col-md-2'>
+        	<h2 class='home-header'>Friday</h2>
+            @if($fridayWorkout)
+                <a class='btn btn-warning home-edit-btn' href="/workouts/{{ $fridayWorkout->id }}"><span class='glyphicon glyphicon-pencil' aria-hidden="true"></span></a>
+                <ul class='list-group'>
+                @foreach($fridayExercises as $exercise)
+                    <li class='list-group-item'>
+                        <h4 class='h4-home'>{{ $exercise->name }}</h4>
+                        <table class='table table-condensed'>
+                            <thead>
+                                <tr>
+                                    <th>Sets</th>
+                                    <th>Reps</th>
+                                    <th>Weight (lbs)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $exercise->sets }}</td>
+                                    <td>{{ $exercise->reps }}</td>
+                                    <td>{{ $exercise->weight }}</td>
+                                </tr>
+                            </tbody>
+                        </table>                    
+                    </li>
+                @endforeach
+                </ul>
+            @else
+                <h4 class='h4-home alert alert-info'>No Friday workout found for this week.</h4>
+            @endif
+        </div>
     </div>
 </div>
 @endsection
